4cd486e10e89d87874418a7d3425c26342cadac5
loic.martel@outlook.com <loic.martel@outlook.com>
2019-06-30 15:40:02 +0200

    some elts for dev

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export {sayHello, sayGoodbye} from './controler'
+export {sayHello, sayGoodbye} from './controler';
+import {sayHello, sayGoodbye} from './controler'; // to del
+//test
+let a = sayHello(); //to del
