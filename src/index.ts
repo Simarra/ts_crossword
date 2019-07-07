529e78f6dd1a64e535cf08a86ef8767927a1fe8d
loic.martel@outlook.com <loic.martel@outlook.com>
2019-07-07 18:50:55 +0200

    Start coding export func

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { WordListDescr } from "./words";
-
-let test_words = new WordListDescr(['maison', 'table'], ['lieu ou habiter', 'surface plate']);
-//test_words.generate_word_descr_array();
-test_words.generate_word_descr_array()
+import { generate_crossword } from "./controler";
