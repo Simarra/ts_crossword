cbb74f9604c0f37edd3f1b075cb7ab9fc0ae5269
loic.martel@outlook.com <loic.martel@outlook.com>
2019-08-22 14:35:46 +0200

    Remap index

--- src/controler.ts
+++ src/controler.ts
@@ -2,11 +2,21 @@ import { Grid } from './grid';
 import { WordListDescr } from './words';
 
 
-export function generate_crossword(word_list: Array<string>, description_list: Array<string>, nb_rows: number, nb_cols: number): Grid{
+export function generate_crossword_using_brute_force(word_list: Array<string>, description_list: Array<string>, nb_rows: number, nb_cols: number): Grid{
     // Generate an array using words, descriptions, and cols and cell numbers.
   let words = new WordListDescr(word_list, description_list);
   let grid = new Grid(nb_rows, nb_cols, words);
-  grid.fill_board()
+  grid.fill_board("brut_force")
+  return grid;
+
+};
+
+
+export function generate_crossword_using_easy_mode(word_list: Array<string>, description_list: Array<string>, nb_rows: number, nb_cols: number): Grid{
+    // Generate an array using words, descriptions, and cols and cell numbers.
+  let words = new WordListDescr(word_list, description_list);
+  let grid = new Grid(nb_rows, nb_cols, words);
+  grid.fill_board("easy_mode")
   return grid;
 
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { generate_crossword } from "./controler";
+export { generate_crossword_using_easy_mode, generate_crossword_using_brute_force } from "./controler";
 
 export { Grid } from './grid';
 export { WordListDescr } from './words';
